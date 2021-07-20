10e02f29866053baeba2167645da5e4de4578275
Pooria4484 <47162303+Pooria4484@users.noreply.github.com>
2021-07-21 03:30:36 +0430

    socket bugs fixed

--- lib/main.dart
+++ lib/main.dart
@@ -4,126 +4,128 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mushroom/home.dart';
 
-
-Client client=Client(39810);
+Client client = Client(39810);
 late Body body;
-void main(){
+
+void main() {
   server();
-  body=Body();
-  runApp(body);
-}
+  body = Body();
+  runApp(MaterialApp(
+      title: 'Navigation Basics',
+      home: body,
+    debugShowCheckedModeBanner: false,
+  ));
+  }
 
-
-class Client{
+class Client {
   late Socket socket;
-  var isConnected=false;
-  var port=39810;
+  var isConnected = false;
+  var port = 39810;
+
   Client(this.port);
 
-
   void startTCPClient() async {
-    valueNotifier.value=Icon(Icons.wifi_protected_setup_sharp,color: Colors.red);
+    valueNotifier.value =
+        Icon(Icons.wifi_protected_setup_sharp, color: Colors.red);
     try {
-      socket = await Socket.connect('192.168.4.1', this.port);
+      socket = await Socket.connect('192.168.4.1', 39810);
       socket.write('Hello, Server!\r\n');
       socket.listen(
-            (Uint8List data) {
+        (Uint8List data) {
           isConnected = true;
           final serverResponse = String.fromCharCodes(data);
           handleMessage(serverResponse);
         },
-
         onError: (error) {
           print(error);
           socket.destroy();
+          client.socket.close();
           isConnected = false;
           valueNotifier.value =
               Icon(Icons.wifi_protected_setup_sharp, color: Colors.red);
         },
-
         onDone: () {
           print('Server left.');
-          socket.destroy();
-          isConnected = false;
-          valueNotifier.value =
-              Icon(Icons.wifi_protected_setup_sharp, color: Colors.red);
+          client.socket.close();
+
+          //socket.destroy();
+          //isConnected = false;
+          //valueNotifier.value =
+              //Icon(Icons.wifi_protected_setup_sharp, color: Colors.red);
         },
       );
-    }catch(e){print(e.toString());}
+    } catch (SocketException) {
+      print(SocketException.toString());
+    }
   }
 
-  void send(data)async{
-    try{
-      socket.write(data+'\r\n');
-    }catch(e){
-
-    }
+  void send(data) async {
+    try {
+      socket.write(data + '\r\n');
+    } catch (e){print(e);}
   }
 
   void handleMessage(message) async {
-    valueNotifier.value=Icon(Icons.wifi_outlined,color: Colors.white);
-    String cmnd=message;
-    if(cmnd.contains('*#')&&cmnd.contains('@\$')){
-      cmnd=cmnd.substring(cmnd.indexOf('*#')+2,cmnd.indexOf('@\$'));
+    valueNotifier.value = Icon(Icons.wifi_outlined, color: Colors.white);
+    String cmnd = message;
+    if (cmnd.contains('*#') && cmnd.contains('@\$')) {
+      cmnd = cmnd.substring(cmnd.indexOf('*#') + 2, cmnd.indexOf('@\$'));
       print(cmnd);
-      if(cmnd.contains('tec')){
-        String buff=cmnd.substring(cmnd.indexOf('tec')+3,cmnd.indexOf('tec')+6);
+      if (cmnd.contains('tec')) {
+        String buff =
+            cmnd.substring(cmnd.indexOf('tec') + 3, cmnd.indexOf('tec') + 6);
         var val = int.parse(buff);
         assert(val is int);
         setTempVal(val);
       }
 
-      if(cmnd.contains('teg')){
-        String buff=cmnd.substring(cmnd.indexOf('teg')+3,cmnd.indexOf('teg')+5);
+      if (cmnd.contains('teg')) {
+        String buff =
+            cmnd.substring(cmnd.indexOf('teg') + 3, cmnd.indexOf('teg') + 5);
         var val = int.parse(buff);
         assert(val is int);
         setTempGoalVal(val);
       }
 
-
-      if(cmnd.contains('hug')){
-        String buff=cmnd.substring(cmnd.indexOf('hug')+3,cmnd.indexOf('hug')+5);
+      if (cmnd.contains('hug')) {
+        String buff =
+            cmnd.substring(cmnd.indexOf('hug') + 3, cmnd.indexOf('hug') + 5);
         var val = int.parse(buff);
         assert(val is int);
-        setTempGoalVal(val);
+        setHuGoalVal(val);
       }
 
-
-      if(cmnd.contains('huc')){
-        String buff=cmnd.substring(cmnd.indexOf('huc')+3,cmnd.indexOf('huc')+6);
+      if (cmnd.contains('huc')) {
+        String buff =
+            cmnd.substring(cmnd.indexOf('huc') + 3, cmnd.indexOf('huc') + 6);
         var val = int.parse(buff);
         assert(val is int);
-        setTempVal(val);
+        setHuVal(val);
       }
-
-    }
-    print(message);
-
-  }
-
-
-}
-
-void server()async{
-  while(true){
-    await Future.delayed(const Duration(seconds:4));
-    try{
-      if(client.isConnected)
-        {
-          client.send('*#sync@\$');
-        }else{
-        client.startTCPClient();
-      }
-    }catch(e){
-      print(e);
     }
   }
-
-
 }
 
+void server() async {
+  while (true) {
+    await Future.delayed(const Duration(seconds: 4));
+    try {
+     // if (client.isConnected) {
+        client.send('*#sync@\$');
+      //} else {
+      //  client.startTCPClient();
+      }
+     catch (e) {
+       print(e);
+     }
+  }
+}
 
-void wifiOnClick(){
-  client.socket.destroy();
-  client.startTCPClient();
+void wifiOnClick() {
+  try {
+    // client.socket.destroy();
+    // client.socket.close();
+    // client = Client(39810);
+    client.startTCPClient();
+  } catch (e){print(e);}
 }
